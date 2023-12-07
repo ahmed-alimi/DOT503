d9e798e691d9e2b26eb99172e145792984120082
ahmed.alimi <alimiahmed79@gmail.com>
2023-12-08 07:11:44 +1030

    Updates

---
+++ test_calculator.py
@@ -0,0 +1,22 @@
+import unittest
+import calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        result = calculator.add(10,5)
+        self.assertEqual(result, 15)
+
+    def test_subtract(self):
+        result = calculator.subtract(10,5)
+        self.assertEqual(result, 5)
+
+    def test_multiply(self):
+        result = calculator.multiply(10,5)
+        self.assertEqual(result, 50)
+
+    def test_divide(self):
+        result = calculator.divide(10,5)
+        self.assertEqual(result, 2)
+
